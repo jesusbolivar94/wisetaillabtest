9df49211fc0d67a7b51bb0fd093d89272f2b8c79
j5rojo <j5rojo94@gmail.com>
2023-02-06 12:45:18 +0000

    Vue frontend for showing stats based on the scrapping data.

---
+++ app/src/components/Dashboard.vue
@@ -0,0 +1,40 @@
+<script setup>
+    import { onMounted } from 'vue'
+    import Chart from 'chart.js/auto'
+    import data from '../../../data.json'
+
+    onMounted(() => {
+        const canvas = document.querySelector('#stats')
+
+        new Chart(
+            canvas,
+            {
+                type: 'line',
+                data: {
+                    labels: Object.keys(data),
+                    datasets: [
+                        {
+                            label: 'MCoutinho Peças',
+                            data: Object.values(data),
+                        }
+                    ],
+                },
+                options: {
+                    scales: {
+                        y: {
+                            min: Math.min( ...Object.values(data) ),
+                            max: Math.max( ...Object.values(data) ),
+                        }
+                    }
+                }
+            }
+        )
+    })
+</script>
+
+<template>
+    <h1 class="font-bold text-3xl">Wise Tail Lab Test</h1>
+    <div class="w-9/12">
+        <canvas id="stats"></canvas>
+    </div>
+</template>
